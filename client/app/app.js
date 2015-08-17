122ce84be01f048eac372db15618312d7e44473f
Gregory Cowan <g.cowan@amnzero.nl>
2015-08-17 17:18:46 +0200

    Continued work

--- client/app/app.js
+++ client/app/app.js
@@ -12,42 +12,138 @@
                 controller: Controller
             };
 
-            function Controller ($timeout) {
+            function Controller ($timeout, $element, $scope) {
+
+                var submitting = false;
+                var validityWatchers = [];
 
                 var self = this;
 
+                self.model = {};
+
                 self.submit = submit;
+                self.setTabIndex = setTabIndex;
 
                 function submit (form) {
 
-                    console.log('submitting');
                     console.log(form);
+
+                    if (submitting) { return; }
+
+                    // first reset all non-blocking errors to valid
+                    form.$setValidity('internalservererror', true);
+                    form.$setValidity('accounttoosimilar', true);
+                    form.phone.$setValidity('phonenotrecognized', true);
+                    form.address.$setValidity('addressnotrecognized', true);
+
+
                     if (!form.$valid) {
+
+                        document.getElementById('errors').focus();
+
+                    }
+                    else {
+
+                        submitting = true;
+
+                        // remove any existing watchers
+                        while (validityWatchers.length) {
+                            validityWatchers.pop()();
+                        }
 
                         $timeout(function () {
 
-                            document.getElementById('errors').focus();
+                            self.model.error = self.model.error || {};
 
-                            var elementList = document.querySelectorAll('#errors a');
+                            if (self.model.error.internalservererror) {
+                                form.$setValidity('internalservererror', false);
+                            }
+                            else if (self.model.error.accounttoosimilar) {
+                                form.$setValidity('accounttoosimilar', false);
+                            }
+                            else {
+                                if (self.model.error.emailinuse) {
 
-                            Object.keys(elementList).forEach(function (elKey) {
+                                    form.email.$setValidity('emailinuse', false);
+                                    var invalidEmailValue = self.model.email;
 
-                                var el = elementList[elKey];
+                                    validityWatchers.push($scope.$watch('form.model.email', function (newEmailValue) {
+                                        if (invalidEmailValue === newEmailValue) {
+                                            form.email.$setValidity('emailinuse', false);
+                                        }
+                                        else {
+                                            form.email.$setValidity('emailinuse', true);
+                                        }
+                                    }));
 
-                                el.addEventListener('click', function (e) {
+                                }
+                                if (self.model.error.phonenotrecognized) {
 
-                                    e.preventDefault();
-                                    document.getElementById(el.getAttribute('data-element-id')).focus();
+                                    form.phone.$setValidity('phonenotrecognized', false);
 
+                                }
+                                if (self.model.error.addressnotrecognized) {
+
+                                    form.address.$setValidity('addressnotrecognized', false);
+
+                                }
+                            }
+
+                            if (form.$valid === false) {
+                                $timeout(function () {
+                                    document.getElementById('errors').focus();
                                 });
+                            }
 
-                            });
 
-                        });
+                            submitting = false;
 
+                        }, 1500);
 
                     }
-                    return false;
+
+                }
+
+                function setTabIndex () {
+
+                    // todo: need to find a clean way to mark form subsections as invalid 
+                    //  and to include those fields in the tab flow, even though fields are not marked
+                    //  with aria-invalid
+
+                    // set tabindex on the form
+                    var elementList = $element[0].querySelectorAll('input[tabindex], [ng-form].ng-invalid');
+
+                    var firstElement;
+
+                    for (var key in elementList) {
+                        if (elementList.hasOwnProperty(key)) {
+
+                            var element = elementList[key];
+
+                            // remove ability to tab to valid form fields
+                            var tabIndex = '-1';
+
+                            // todo: might be better to look for aria-invalid?
+                            if (element.className.match(/(^|\s)ng-invalid(\s|$)/) ||
+                                element.getAttribute('ng-messages') !== null) {
+                                // if field is invalid, tabIndex should be reset to '0'
+                                tabIndex = '0';
+
+                                if (typeof firstElement === 'undefined') {
+                                    firstElement = element;
+                                }
+                            }
+
+                            element.setAttribute('tabindex', tabIndex);
+
+                        }
+                    }
+
+                    firstElement.focus();
+
+                }
+
+                function restoreTabIndex () {
 
                 }
 
--- readme.md
+++ readme.md
@@ -12,8 +12,19 @@ Some wild ideas are scribbled down in `flow.md`. May contain obsolete informatio
 
 ## Learned
 
+- Accessibility may also need UX input -- what practices give disabled users a good user experience with the tools they use? This requires experience with the tools. Examples may be a good starting point. 
 - `aria-live` attribute is read by screen reader as soon as a change is detected
 - `aria-live` changes on multiple elements will not read all changes if changes occur at the same time
-- `aria-live` elements can receive focus, no `tab-index` is required
+- `tabindex="-1"` is required for a `div` to be able to receive focus in Chrome (value may differ)
+- `aria-live` on a div is enough for Safari to allow focus
+- `tabindex` marks content as `role=group` and changes screen reader behaviour
 - `display: hidden` is excluded from screen readers
 - content clipped using `css` `clip` is still focusable and interferes with tab order for non-disabled users
+- `tabindex="0"` follows default tab order
+- nested tabindices do not seem to be supported out of the box
+- marking an input with an error using `aria-describedby` gives the user feedback about an error when focusing on a form field (needs some idle time for the screen reader to use this)
+- 
+
+## Wondering
+
+- Angular Material uses `ng-messages` to implement on the fly error feedback for screen readers, however no notification is given when the field changes from valid to invalid, is this an issue?
