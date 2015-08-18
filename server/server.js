0c5778a1501290c2f7f7c53c466e273041a8a051
Gregory Cowan <g.cowan@amnzero.nl>
2015-08-18 16:59:09 +0200

    Fixes for CloudFoundry deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,6 @@ var app = $express();
 app.use('/static/', $express.static($upTheTree() + '/bower_components'));
 app.use('/', $express.static($upTheTree() + '/client'));
 
-app.listen(8989, function () {
+app.listen(process.env.VCAP_APP_PORT || 8989, function () {
     console.log('localhost:8989');
 });
